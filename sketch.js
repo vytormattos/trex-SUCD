4020009feb508db180c3991e888df0a75f4ebe01
vytormattos <126834619+vytormattos@users.noreply.github.com>
2023-04-06 20:05:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+
+//preload carrega as midías do jogo 
+function preload(){
+
+  
+}
+//setup faz a aconfiguração
+function setup(){
+  createCanvas(600,200);
+  // criando as bordas
+  
+}
+//draw faz o movimento, a ação do jogo
+function draw(){
+  background("#f0f9f7");
+  
+ 
+ 
+ 
+   //coordenadas do mouse na tela
+  text("X: "+mouseX+"/ Y: "+mouseY,mouseX,mouseY);
+  drawSprites();
+
+}
